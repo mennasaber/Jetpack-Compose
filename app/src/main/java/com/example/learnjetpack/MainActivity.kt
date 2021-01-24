c4a69822ba3b2acf93b51248a12580e67ccd9638
mennasaber <mennasaber0193@gmail.com>
2021-01-24 21:51:40 +0200

    Row Layout

--- app/src/main/java/com/example/learnjetpack/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpack/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.imageFromResource
@@ -16,6 +18,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +39,26 @@ class MainActivity : AppCompatActivity() {
                         contentScale = ContentScale.Fit,
                 )
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                            text = "Name: Product-1",
-                            style = TextStyle(
-                                    fontSize = TextUnit.Companion.Sp(21),
-                                    fontWeight = FontWeight.Bold,
-                            )
-                    )
+                    Row(modifier = Modifier
+                            .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                                text = "Name: Product-1",
+                                style = TextStyle(
+                                        fontSize = TextUnit.Companion.Sp(21),
+                                        fontWeight = FontWeight.Bold,
+                                )
+                        )
+                        Text(
+                                text = "Price: 100$",
+                                style = TextStyle(
+                                        fontSize = TextUnit.Companion.Sp(17),
+                                        color = Color.Gray,
+                                ),
+                                modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                        )
+                    }
                     Spacer(modifier = Modifier.padding(top = 10.dp))
                     Text(
                             text = "Colors: Red - Blue",
@@ -50,14 +66,16 @@ class MainActivity : AppCompatActivity() {
                                     fontSize = TextUnit.Companion.Sp(17),
                             )
                     )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(
-                            text = "Price: 100$",
-                            style = TextStyle(
-                                    fontSize = TextUnit.Companion.Sp(17),
-                                    color = Color.Gray,
-                            )
-                    )
+                    Spacer(modifier = Modifier.padding(top = 16.dp))
+                    Button(
+                            onClick = { },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(
+                                text = "Add To Cart",
+                                style = TextStyle(color = Color.White)
+                        )
+                    }
                 }
             }
         }
